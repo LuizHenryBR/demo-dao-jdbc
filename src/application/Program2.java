f0fb2a74d1fcd3780834b8aa81c9f1788c9c5b67
LuizHenryBR <siqueiralh7768@hotmail.com>
2023-12-01 02:45:50 -0300

    Department deleteById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -13,11 +13,11 @@ public class Program2 {
 	public static void main(String[] args) {
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
-		
-		//System.out.println("=== TEST1: Department insert ===");
-		//Department newDep = new Department(null, "Marketing");
-		//depDao.insert(newDep);
-		//System.out.println("NEW:" + newDep.getName() + " department inserted! ID: " + newDep.getId());
+
+		System.out.println("=== TEST1: Department insert ===");
+		Department newDep = new Department(null, "Marketing");
+		depDao.insert(newDep);
+		System.out.println("NEW:" + newDep.getName() + " department inserted! ID: " + newDep.getId());
 
 		System.out.println("\n=== TEST2: Department findByID ===");
 		Department fDep = depDao.findById(6);
@@ -30,8 +30,14 @@ public class Program2 {
 		for (Department d : listAll){
 			System.out.println(d);
 		}
+
+		System.out.println("\n=== TEST4: Department findByID ===");
+		int idToDelete = 9;
+		depDao.deleteById(7);
+		depDao.deleteById(8);
+		depDao.deleteById(10);
+		depDao.deleteById(11);
+		System.out.println("CAUTION! The departmente with id=" + idToDelete + " was deleted");
 		
 	}
-	
-
 }
